69177fbbbbda89206a74a44c0623211fd0b3b7ff
ChaserZ98 <feiyuzheng98@gmail.com>
2024-06-04 20:16:40 -0400

    ✨ feat: Add SQLite database engine and some helper models

---
+++ src/Fisher/db/models.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from datetime import datetime
+
+from sqlalchemy import TIMESTAMP, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func
+
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
+    deleted_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True), nullable=True
+    )
+
+
+class IdMixin:
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
