ae28f4bfef097255c1d788d196003816da594241
ChaserZ98 <feiyuzheng98@gmail.com>
2024-06-10 03:05:00 -0400

    ✨ feat: Update TimestampMixin column type to Datetime

--- src/Fisher/db/models.py
+++ src/Fisher/db/models.py
@@ -2,24 +2,22 @@ from __future__ import annotations
 
 from datetime import datetime
 
-from sqlalchemy import TIMESTAMP, Integer
+from sqlalchemy import DateTime, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import func
 
 
 class TimestampMixin:
     created_at: Mapped[datetime] = mapped_column(
-        TIMESTAMP(timezone=True), nullable=False, server_default=func.now()
+        DateTime(timezone=True), nullable=False, server_default=func.now()
     )
     updated_at: Mapped[datetime] = mapped_column(
-        TIMESTAMP(timezone=True),
+        DateTime(timezone=True),
         nullable=False,
         server_default=func.now(),
         onupdate=func.now(),
     )
-    deleted_at: Mapped[datetime] = mapped_column(
-        TIMESTAMP(timezone=True), nullable=True
-    )
+    deleted_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=True)
 
 
 class IdMixin:
